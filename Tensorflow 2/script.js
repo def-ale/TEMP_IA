7e289daa3f949c289439a0003aff4acdd71f66d1
Alexandre Ferreira <alexandrefbertolin@gmail.com>
2024-04-24 21:48:06 -0300

    Aula 24 de abril

--- Tensorflow 2/script.js
+++ Tensorflow 2/script.js
@@ -45,6 +45,72 @@ function createModel() {
     return model;
 }
 
-const model = createModel();
+
+
+function convertToTensor(data) {
+    
+    return tf.tidy( () => {
+        // Passo 1: embaralhe os dados
+        tf.util.shuffle(data);
+        
+        // Passo 2: converta dados em tensor
+        const inputs = data.map((d) => d.horsepower);
+        const labels = data.map((d) => d.mpg);
+
+        const inputTensor = tf.tensor2d(inputs, [inputs.lenght, 1]);
+        const labelsTensor = tf.tensor2d(labels, [labels.lenght, 1]);
+
+        // Passo 3: Normalize os dados para o intervalo 0 - 1 usando escala min-max
+        const inputMax = inputTensor.max();
+        const inputMin = inputTensor.min();
+        const labelMax = labelsTensor.max();
+        const labelMin = labelsTensor.min();
+
+        // Essa operação é a escala min-max (retorna um número entre 0 e 1)
+        const normalizedInputs = inputTensor
+            .sub(inputMin)
+            .div(inputMax.sub(inputMin));
+
+        const normalizedLabels = labelsTensor
+            .sub(labelMin)
+            .div(labelMax.sub(labelMin));
+
+        return {
+            inputs: normalizedInputs,
+            labels: normalizedLabels,
+            // Retorne os limites mínimo/máximo para que posamos usá-los mais tarde.
+            inputMax: inputMax,
+            inputMin: inputMin,
+            labelMax: labelMax,
+            labelMin: labelMin
+        };
+
+    });
+}
+
+async function trainModel(model, inputs, labels) {
+    // Prepara o modelo para o treinamento.
+    model.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError,
+        metrics: ["mse"]
+    });
+
+    const batchSize = 32;
+    const epochs = 50;
+
+    return await model.fit(inputs, labels, {
+        batchSize,
+        epochs,
+        shuffle: true,
+        callbacks: tfvis.show.fitCallbacks(
+            { name: "Performance do treinamento" },
+            [ "loss", "mse" ],
+            { height: 200, callbacks: ["onEpochEnd"] }
+        ),
+    });
+}
 
 tfvis.show.modelSummary({name: "Modelo"}, model);
+
+const model = createModel();
